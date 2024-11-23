66951de33deb502d3a4de8a6b03cee705481687e
V1nay-18 <147505720+V1nay-18@users.noreply.github.com>
2024-11-23 22:44:47 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;  
 
-
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use(express.json());
 
 app.use("/api/v1", router);
